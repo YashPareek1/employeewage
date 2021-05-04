27f0af0f75f3ebdb5f2d9aafefab1e7154d2e3bd
YashPareek1 <yashpareekg4@gmail.com>
2021-05-05 00:22:27 +0530

    Solving using

--- Employeewagebuilder.java
+++ Employeewagebuilder.java
@@ -1,23 +1,26 @@
 
 public class Employeewagebuilder {
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to employee wage");
-		
-		//constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int EMP_RATE_PER_HOUR = 20;
-        //variables
+	public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static void main(String[] args)
+    {
         int empHrs = 0;
         int empWage = 0;
-        //computation
-        double empCheck = Math.floor(Math.random() * 10) % 3;
-        if (empCheck == IS_FULL_TIME)
-            empHrs = 6;
-        else if (empCheck == IS_PART_TIME)
-            empHrs = 8;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+        switch (empCheck)
+        {
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            default :
+                empHrs = 0;
+        }
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Emp Wage: " + empWage);
-	}
+    }
 }
